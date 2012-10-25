688fba198802f8857cb914bf1a758310503164a3
Roland Kaiser <kardinal.eros@gmail.com>
2012-10-25 13:58:15 +0200

    copied R headers

---
+++ Headers/i386/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 134912
+#define R_NICK ""
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "2"
+#define R_MINOR  "15.0"
+#define R_STATUS ""
+#define R_YEAR   "2012"
+#define R_MONTH  "03"
+#define R_DAY    "30"
+#define R_SVN_REVISION "58871"
+#define R_FILEVERSION    2,150,58871,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
